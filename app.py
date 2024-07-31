09a7c4eca9c048bfd7c3901d9aa5fcd5276c3130
benaleobayu <benaleobs@gmail.com>
2024-08-01 00:41:04 +0700

    add flask migrate

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from flask_migrate import Migrate
+from config import Config
+from models import db
 from flask import Flask
 from utils.check_db import connection_check
 
@@ -5,9 +8,17 @@ from routes.UserRoute import user_bp
 
 app = Flask(__name__)
 
+app.config.from_object(Config)
+
+
 app.register_blueprint(user_bp, url_prefix='/api')
 
 connection_check()
+
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
 @app.route('/')
 def index():
     return "Helo everybody, this is a Final Project"
