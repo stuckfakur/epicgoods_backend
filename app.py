d683fb25efa5d837e48c6752fa4b3bcaf534349b
benaleobayu <benaleobs@gmail.com>
2024-08-01 19:42:01 +0700

    change the order

--- app.py
+++ app.py
@@ -7,17 +7,15 @@ from utils.check_db import connection_check
 from routes.UserRoute import user_bp
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
 
+db.init_app(app)
+migrate = Migrate(app, db)
 
 app.register_blueprint(user_bp, url_prefix='/api')
 
 connection_check()
 
-
-db.init_app(app)
-migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
