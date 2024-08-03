03aea86c3231dbd7eef2cdc0dc5186476fd9a4c2
kholikhub <kholik.work@gmail.com>
2024-08-03 09:51:33 +0700

    db model user complete

--- models/User.py
+++ models/User.py
@@ -1,24 +1,32 @@
 from . import db
 from flask_login import UserMixin
+import bcrypt
+from sqlalchemy import ForeignKey
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(80), nullable=False)
     username = db.Column(db.String(80), nullable=False, unique=True)
     email = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-    # seller_id = db.Column(db.Integer, ForeignKey("seller.id"))
-    status = db.Column(db.String(2), nullable=False)
+    consumer_data =db.Column(db.Text)
+    isSeller = db.Column(db.String(2), default='0')
+    seller_id = db.Column(db.Integer, ForeignKey("seller"))
+    status = db.Column(db.String(2), default='1')
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     
     def to_dict(self):
         return{
             'id' : self.id,
+            'name': self.name,
             'username' : self.username,
             'email' : self.email,
             'password' : self.password,
-            # 'seller_id' : self.seller_id,
+            'consumer_data' : self.consumer_data,
+            'isSaller' : self.isSeller,
+            'seller_id' : self.seller_id,
             'status' : self.status,
             'created_at' : self.created_at,
             'updated_at' : self.updated_at
