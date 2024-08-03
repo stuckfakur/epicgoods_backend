74c4afa3d90e60072a405dc7a4915a1e1a8db3be
kholikhub <kholik.work@gmail.com>
2024-08-03 12:23:15 +0700

    add api create user

--- repositories/UserRepository.py
+++ repositories/UserRepository.py
@@ -5,3 +5,43 @@ class UserRepository:
     @staticmethod
     def get_all_users():
         return User.query.all()
+    
+    @staticmethod
+    def get_user_by_id(id):
+        return User.query.get(id)
+
+    @staticmethod
+    def create_user(
+        name,
+        username,
+        email,
+        password,
+        consumer_data
+    ):
+       new_user = User(
+           name = name,
+           username = username,
+           email = email,
+           consumer_data = consumer_data
+       )
+       new_user.set_password(password)
+       db.session.add(new_user)
+       db.session.commit()
+       return new_user
+
+    @staticmethod
+    def update_user(id, data):
+        user = User.query.get(id)
+        if user:
+            for key, value in data.items():
+                setattr(user, key, value)
+            db.session.commit()
+        return user
+
+    @staticmethod
+    def delete_user(id):
+        user = User.query.get(id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+        return user
--- routes/UserRoute.py
+++ routes/UserRoute.py
@@ -7,7 +7,11 @@ user_bp = Blueprint('user_bp', __name__)
 class UserForm:
     def __init__(self):
         data = request.get_json()
+        self.name = data.get('name')
         self.username = data.get('username')
+        self.email = data.get('email')
+        self.password = data.get('password')
+        self.consumer_data = data.get('consumer_data')
 
 
 @user_bp.route('/users', methods=['GET'])
@@ -17,3 +21,42 @@ def api_get_users():
         'status': 200,
         'data': users
     }) if users else ('', 404)
+
+@user_bp.route('/users', methods=['POST'])
+def api_create_users():
+    form = UserForm()
+    users = UserService.create_users(
+        form.name,
+        form.username,
+        form.email,
+        form.password,
+        form.consumer_data
+    )
+    return jsonify({
+        'status': 201,
+        'data': users.to_dict()
+    }) if users else ('', 400)
+
+@user_bp.route('/users/<int:id>', methods=['GET'])
+def api_get_users_by_id(id):
+    users = UserService.get_users(id)
+    return jsonify({
+        'status': 200,
+        'data': users
+    }) if users else ('', 400)
+
+@user_bp.route("/users/<int:id>", methods=["DELETE"])
+def api_delete_users(id):
+    users = UserService.delete_users(id)
+    return jsonify({
+        'status': 200,
+        'data': users
+    }) if users else ('', 400)
+
+@user_bp.route("/users/<int:id>", methods=["PUT"])
+def api_update_users(id):
+    users = UserService.update_users(id)
+    return jsonify({
+        'status': 200,
+        'data': users
+    }) if users else ('', 400)
