da034c7642bc9063c8cdf0dd910fb4c2bfb33e14
kholikhub <kholik.work@gmail.com>
2024-08-03 12:22:51 +0700

    add api create user

--- services/UserService.py
+++ services/UserService.py
@@ -5,3 +5,31 @@ class UserService:
     def get_all_users():
         users = UserRepository.get_all_users()
         return [user.to_dict() for user in users]
+    
+    @staticmethod
+    def get_user_by_id(id):
+        return UserRepository.get_user_by_id(id)
+
+    @staticmethod
+    def create_users(
+        name,
+        username,
+        email,
+        password,
+        consumer_data
+    ):
+        return UserRepository.create_user(
+            name,
+            username,
+            email,
+            password,
+            consumer_data
+        )
+
+    @staticmethod
+    def update_user(user_id, data):
+        return UserRepository.update_user(user_id, data)
+
+    @staticmethod
+    def delete_user(user_id):
+        return UserRepository.delete_user(user_id)
