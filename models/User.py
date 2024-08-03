2f104dd76eac562cd72defbe419ecb535b386705
kholikhub <kholik.work@gmail.com>
2024-08-03 09:50:53 +0700

    add user model

--- models/User.py
+++ models/User.py
@@ -7,14 +7,30 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80), nullable=False, unique=True)
     email = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    # seller_id = db.Column(db.Integer, ForeignKey("seller.id"))
+    status = db.Column(db.String(2), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     
     def to_dict(self):
         return{
             'id' : self.id,
-            'username' : self.username
+            'username' : self.username,
+            'email' : self.email,
+            'password' : self.password,
+            # 'seller_id' : self.seller_id,
+            'status' : self.status,
+            'created_at' : self.created_at,
+            'updated_at' : self.updated_at
         }
     
     
-    
+
     def __repr__(self) -> str:
         return f"<User {self.name}>"
+
+    def set_password(self, password):
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
