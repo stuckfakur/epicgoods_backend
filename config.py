0c820d665a5c653e3e00ec2d0c7c04554aff3cc0
benaleobayu <benaleobs@gmail.com>
2024-08-10 12:28:25 +0700

    update config

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 
-import dotenv
-from mailjet_rest import Client
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -19,20 +17,6 @@ class Config:
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
 
-    # Mail Settings
-    MAIL_DEFAULT_SENDER = "noreply@epicgoods.com"
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_DEBUG = False
-    MAIL_USERNAME = os.getenv("EMAIL_USER")
-    MAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
-
-    API_KEY = os.environ['MJ_APIKEY_PUBLIC']
-    API_SECRET = os.environ['MJ_APIKEY_PRIVATE']
-
-    mailjet = Client(auth=(API_KEY, API_SECRET))
 
     # Print the database URI for debugging
 print(Config.SQLALCHEMY_DATABASE_URI)
